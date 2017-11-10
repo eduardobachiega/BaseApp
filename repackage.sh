3cfea4312570f3dad8d9a4073e44e68936c11501
Bachiega <eduardo.bachiega@ms2dev.com.br>
2017-11-10 15:37:59 -0200

    Script permission and rename fix

--- repackage.sh
+++ repackage.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-set -x
 sed -i "" "s/applicationId \"eduardobachiega.baseapp\"/applicationId \"$1\"/g" app/build.gradle
 sed -i "" "s/package=\"eduardobachiega.baseapp/package=\"$1/g" app/src/main/AndroidManifest.xml
 appPath=$(echo -n $1 | sed "s/\./\//g")
 mkdir -p app/src/main/java/$appPath
 mv app/src/main/java/eduardobachiega/baseapp/* app/src/main/java/$appPath
 rm -rf app/src/main/java/eduardobachiega
-find . -name "*.java" -exec sed -i "" "s/$appPath/g" {} +
+find . -name "*.java" -exec sed -i "" "s/eduardobachiega.baseapp/$1/g" {} +
